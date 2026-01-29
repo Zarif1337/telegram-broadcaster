61eb6e33186418739b64be965d7608ee8dd4ec8a
Zarif1337 <zakirulislam448@gmail.com>
2026-01-29 16:56:32 +0600

    main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ generation_config = {
 }
 
 model = genai.GenerativeModel(
-  model_name="gemini-1.5-flash",
+  model_name="gemini-3-flash-preview",
   generation_config=generation_config,
   system_instruction=system_prompt
 )
